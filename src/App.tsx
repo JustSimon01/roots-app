0a92063f12379a8466645ed544a3d3bfef545ff1
JustSimon01 <simon.ch@yandex.ru>
2023-06-04 23:21:34 +0300

    карточка персоны для главной страницы

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import SideMenu from './components/SideMenu/SideMenu';
+import Card from './components/Сard/Card';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +11,12 @@ function App() {
   return (
     <>
       <SideMenu header={'Roots'} />
-      <div className="main-block"></div>
+      <div className="main-block">
+        <Card
+          img={'https://www.cartonionline.com/tv/boing/Gennaio/Johnny_Bravo_Pointing_3.jpg'}
+          name={'Джонни Браво'}
+        />
+      </div>
     </>
   );
 }
